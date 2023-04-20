62f493c373eed1d92217a6ccc478c88c8b8b3c41
Vadim <vadim10zhur@gmail.com>
2023-04-20 21:56:28 +0300

    add class music_player and class model

---
+++ source/music_player.py
@@ -0,0 +1,108 @@
+import pygame
+import tkinter
+import os
+import webbrowser
+
+from recommendation_model import RecommendationModel
+
+
+class MusicPlayer:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Music Player")
+        self.root.geometry("500x700")
+        pygame.init()
+        pygame.mixer.init()
+        self.find_song_var = tkinter.StringVar()
+        self.recommend_music_var = tkinter.StringVar()
+
+        title = tkinter.Label(self.root, text="Music Player", bd=5, relief=tkinter.GROOVE,
+                              font=("times new roman", 20, "bold"), bg="white", fg="green")
+        title.pack(side=tkinter.TOP, fill=tkinter.X)
+
+        self.find_song_entry = tkinter.Entry(self.root, textvariable=self.find_song_var, font=('calibre', 10, 'normal'))
+        self.find_song_entry.pack()
+        self.song_box = tkinter.Listbox(self.root, bg="white", fg="black", width=50)
+        self.song_box.pack(pady=10)
+
+        self.recommend_music_entry = tkinter.Entry(self.root, textvariable=self.recommend_music_var, font=('calibre', 10
+                                                                                                           , 'normal'))
+        self.recommend_music_entry.pack()
+        self.recommended_music_box = tkinter.Listbox(self.root, bg="white", fg="black", width=50)
+        self.recommended_music_box.pack(pady=10)
+
+        main_menu = tkinter.Menu(self.root)
+        root.config(menu=main_menu)
+        add_song_menu = tkinter.Menu(main_menu)
+        main_menu.add_cascade(label='add song', menu=add_song_menu)
+        add_song_menu.add_command(label='download song', command=self.download_song)
+        add_song_menu.add_command(label='choose my songs', command=self.choose_song)
+
+        play_button = tkinter.Button(self.root, text="Play Song", font=("Helvetica", 10), command=self.play)
+        play_button.pack(pady=5)
+        stop_button = tkinter.Button(self.root, text="Stop Song", font=("Helvetica", 10), command=self.stop)
+        stop_button.pack(pady=5)
+        find_button = tkinter.Button(self.root, text="Find Song", font=("Helvetica", 10), command=self.find_song)
+        find_button.pack(pady=5)
+        pause_button = tkinter.Button(self.root, text="Pause Song", font=("Helvetica", 10), command=self.pause)
+        pause_button.pack(pady=5)
+        unpause_button = tkinter.Button(self.root, text="Unpause Song", font=("Helvetica", 10), command=self.unpause)
+        unpause_button.pack(pady=5)
+        recommend_button = tkinter.Button(self.root, text="Recommend", font=("Helvetica", 10), command=self.recommend)
+        recommend_button.pack(pady=5)
+
+    def download_song(self):
+        webbrowser.open("https://myfreemp3.fm/")
+
+    def choose_song(self):
+        '''
+        songs = filedialog.askopenfilenames(initialdir='D:/Music/', title="Choose a song",
+                                            filetypes=(("mp3 files", "*.mp3"),))
+
+        for song in songs:
+            song = song.replace("D:/Music/", "")
+            song = song.replace(".mp3", "")
+            song_box.insert(END, song)
+        '''
+
+        for song in os.listdir("D:/Music/"):
+            song = song.replace(".mp3", "")
+            self.song_box.insert('end', song)
+
+    def find_song(self):
+        song_to_find = self.find_song_entry.get()
+
+        self.song_box.select_clear(first=0, last=tkinter.END)
+
+        song_index = 0
+        for song in self.song_box.get(0, tkinter.END):
+            if song == song_to_find:
+                self.song_box.select_set(first=song_index, last=song_index)
+            song_index += 1
+
+    def play(self):
+        song = self.song_box.get(self.song_box.curselection())
+        song = f'D:/Music/{song}.mp3'
+
+        pygame.mixer.music.load(song)
+        pygame.mixer.music.play(loops=0)
+
+    def stop(self):
+        pygame.mixer.music.stop()
+
+    def pause(self):
+        pygame.mixer.music.pause()
+
+    def unpause(self):
+        pygame.mixer.music.unpause()
+
+    def recommend(self):
+        favourite_music = self.recommend_music_entry.get()
+
+        my_music = self.song_box.get(0, tkinter.END)
+        model = RecommendationModel(my_music)
+        result = model.recommend_music(favourite_music)
+
+        for song in result.values:
+            song_ = song[0]
+            self.recommended_music_box.insert('end', song_)
---
+++ source/recommendation_model.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.cluster import KMeans
+import warnings
+warnings.filterwarnings('ignore')
+
+
+class RecommendationModel:
+    def __init__(self, user_music):
+        # self.user_info = user_info
+        self.user_music = user_music
+
+    def recommend_music(self, favourite_music):
+        tracks_df = pd.read_csv(
+            'D:/Python projects/Music_Player/recommendation_system/spotify_genius_track_dataset/Data '
+            'Sources/spotify_tracks.csv')
+        kmeans_model = KMeans(n_clusters=5)
+        kmeans_model.fit(tracks_df[['danceability', 'instrumentalness', 'energy', 'tempo', 'valence']])
+        tracks_df['type'] = kmeans_model.labels_
+        tracks_df.to_csv(
+            'D:/Python projects/Music_Player/recommendation_system/spotify_genius_track_dataset/Data '
+            'Sources/clustered_spotify_tracks.csv')
+        # ids = input('Enter comma-separated ids of your favorite songs\n> ').strip().split(',')
+        ids = favourite_music.strip().split(',')
+
+        # search the specified ids in this dataset and get the tracks
+        favorites = tracks_df[tracks_df.id.isin(ids)]
+
+        # code to sort find out the maximum occurring cluster number according to user's favorite track types
+        cluster_numbers = list(favorites['type'])
+        clusters = {}
+        for num in cluster_numbers:
+            clusters[num] = cluster_numbers.count(num)
+
+        # sort the cluster numbers and find out the number which occurs the most
+        user_favorite_cluster = [(k, v) for k, v in sorted(clusters.items(), key=lambda item: item[1])][0][0]
+
+        # finally get the tracks of that cluster
+        suggestions = tracks_df[tracks_df.type == user_favorite_cluster]
+        result_recommendation = suggestions[['name']].loc[suggestions['id'].isin(suggestions['id'].head(n=7))]
+
+        return result_recommendation
