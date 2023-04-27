2d174db5f0a4ca4a421b3f79f160b2397c7fb351
Vadim <vadim10zhur@gmail.com>
2023-04-27 14:49:42 +0300

    inject package with business logic to app

--- source/main.py
+++ source/main.py
@@ -1,7 +1,8 @@
 # from music_player import MusicPlayer
 from view_music_player import ViewMusicPlayer
-from control_music_player import ControlMusicPlayer
-
+# from control_music_player import ControlMusicPlayer
+# import class from my package
+from musicplayercontrolpackage.control_music_player import ControlMusicPlayer
 
 player = ControlMusicPlayer()
 gui = ViewMusicPlayer(player)
